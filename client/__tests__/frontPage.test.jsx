9ffce5bde090b95608eb110bb92c40c89fa4728b
sani-bagh <saba012@student.kristiania.no>
2022-05-11 11:52:33 +0200

    fronPage test 🟢

---
+++ client/__tests__/frontPage.test.jsx
@@ -0,0 +1,33 @@
+import { FrontPage } from "../frontPage";
+import React from "react";
+import { MemoryRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+
+describe("FrontPage component", () => {
+  it("should test  div element", async function () {
+    const domEl = document.createElement("div");
+    const root = createRoot(domEl);
+    root.render(
+      <MemoryRouter>
+        <FrontPage />
+      </MemoryRouter>
+    );
+    expect(domEl.innerHTML).toMatchSnapshot();
+  });
+
+  it("should test text in link element", function () {
+    const linkEl = document.createTextNode(/log in/i);
+    expect(linkEl).toMatchSnapshot();
+  });
+
+  it("should test link element", async function () {
+    const linkEl = document.createElement("Link");
+    const root = createRoot(linkEl);
+    root.render(
+      <MemoryRouter>
+        <FrontPage />
+      </MemoryRouter>
+    );
+    expect(linkEl.innerHTML).toMatchSnapshot();
+  });
+});
