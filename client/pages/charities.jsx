905244ab6c6d83a669c75c19d85731f82d92d117
leaskagen <50326406+leaskagen@users.noreply.github.com>
2022-05-23 20:09:24 +0200

    Styled charities page

--- client/pages/charities.jsx
+++ client/pages/charities.jsx
@@ -2,19 +2,26 @@ import React, { useContext } from "react";
 import { ApiContext } from "../apiContext";
 import { useLoading } from "../useLoading";
 
+import PlaceholderBackground from "../pics/bg-pictures/charities-placeholder.jpg";
+import PlaceholderLogo from "../pics/bg-pictures/charities-logo-placeholder.png";
+
 function CharityCard({charity: {name, description}}){
+
+    // Replace placeholders with actual charities images
   return (
-    <div className="flex-container">
       <div className={"charity-card"}>
-        <p>{name}</p>
-        <p>{description}</p>
-        <div className={"card-buttons"}>
-          <button className="card-btn-left card-btn">Read more</button>
-          <button className="card-btn-right card-btn">Donate</button>
+        <div className={"charity-background"} style={{backgroundImage: `url( ${PlaceholderBackground} )`}}>
+          <img className={"charity-img"} src={PlaceholderLogo}/>
+        </div>
+        <div className={"charity-card-content"}>
+          <h3>{name}</h3>
+          <p>{description}</p>
+          <div className={"card-buttons"}>
+            <button className="card-btn-left card-btn">Read more</button>
+            <button className="card-btn-right card-btn">Donate</button>
+          </div>
         </div>
       </div>
-
-    </div>
   );
 }
 
@@ -37,10 +44,11 @@ export function Charities() {
     );
   }
 
+  // Add category filter
   return (
     <div>
       <div>
-        <ul>
+        <ul className={"charity-parent-cont"}>
           {data.map((charity, index) => (
             <CharityCard key={index} charity={charity} />
           ))}
