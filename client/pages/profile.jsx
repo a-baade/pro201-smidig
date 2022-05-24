36ad8846e550049007e495fecf59f9b3e7aa2ad5
ncdalen <78857275+ncdalen@users.noreply.github.com>
2022-05-24 05:47:35 +0200

    basic profile setup (not finished)

--- client/pages/profile.jsx
+++ client/pages/profile.jsx
@@ -1,17 +1,45 @@
 import React from "react";
+import MainBg from "../pics/about-us-pictures/main-bg.png";
 
 export function Profile({ user }) {
   return (
-    <div>
-      <h1>Profile</h1>
-      <div>
-        <div>
-          <h3>Name: {user.google.name}</h3>
-          <h3>Email: {user.google.email}</h3>
+    <div className={"wrap"}>
+      <div className={"main-card"}>
+          <div className={"color-block"}></div>
+          <div className={"first-row"}>
+              <div className={"img-btn"}>
+                  <div className={"mock-image"}></div>
+                  <button>Upload photo</button>
+              </div>
+              <h2>Dyrevernalliansen</h2>
+          </div>
+          <div className={"second-row"}>
+              <div className={"company-contact"}>
+                  <h3>company contact</h3>
+                  <p>Kari</p>
+                  <p>Nordmann</p>
+                  <p>+47 95262419</p>
+                  <p>ceo assistant</p>
+              </div>
+              <div className={"payment-details"}>
+                  <h3>payment details</h3>
+                  <p>Kari</p>
+                  <p>Nordmann</p>
+                  <p>12345667890</p>
+                  <p>1234 4567 4567 6789</p>
+              </div>
+          </div>
+          <div className={"third-row"}>
+              <button>Update</button>
+              <button>Sign out</button>
+          </div>
+          {/*<div>
+            <h3>Name: {user.google.name}</h3>
+            <h3>Email: {user.google.email}</h3>
         </div>
-        <div>
-          <img src={user.google.picture} alt="Profile picture" />
-        </div>
+          <div>
+              <img src={user.google.picture} alt="Profile picture" />
+        </div>*/}
       </div>
     </div>
   );
