016f367236bb3f0b7f910ec852ba87fba3a72153
leaskagen <50326406+leaskagen@users.noreply.github.com>
2022-05-27 17:25:40 +0200

    charity pages styled

--- client/pages/charityPage.jsx
+++ client/pages/charityPage.jsx
@@ -1,15 +1,78 @@
 import {ApiContext} from "../apiContext";
 import {useLoading} from "../useLoading";
-import {useContext} from "react";
-import {useSearchParams} from "react-router-dom";
+import React, {useContext} from "react";
+import {Link, useSearchParams} from "react-router-dom";
 import PlaceholderBackground from "../pics/bg-pictures/charities-placeholder.jpg";
 import PlaceholderLogo from "../pics/bg-pictures/charities-logo-placeholder.png";
+import NewsImg1 from "../pics/bg-pictures/img_3.png";
+import NewsImg2 from "../pics/bg-pictures/img_4.png";
+import NewsImg3 from "../pics/bg-pictures/img_5.png";
+
+import Facebook from "../pics/icons/facebook.png";
+import Instagram from "../pics/icons/instagram.png";
+import Snapchat from "../pics/icons/snapchat.png";
+import Tiktok from "../pics/icons/tiktok.png";
+import WWW from "../pics/icons/world-wide-web.png";
+
+import Arrow from "../pics/icons/arrow.png";
 
 function Charity({charity: {name, description}}){
 
     // Replace placeholders with actual charities images
     return (
-        <p>{name}</p>
+        <div>
+            <Link to={"/charities"}>
+                <div className={"charity-back-button"}>
+                    <img src={Arrow}/>
+                </div>
+            </Link>
+            <img src={PlaceholderBackground} className={"charity-page-background"}/>
+            <div className={"charity-page-content"}>
+                <div className={"charity-page-description"}>
+                    <div className={"charity-description-top"}>
+                        <img src={PlaceholderLogo} className={"charity-description-logo"}/>
+                        <h2>{name}</h2>
+                    </div>
+                    <div className={"charity-description-text"}>
+                        <p>{description}</p>
+                    </div>
+                </div>
+                <div className={"charity-page-side"}>
+                    <Link to={"/"}>
+                        <button className={"charity-page-donate-button"}>Donate</button>
+                    </Link>
+                    <a href={"https://www.google.com/"} className={"charity-social-links"}><img src={WWW}/> examplecharity.com</a>
+                    <a href={"https://www.facebook.com/"} className={"charity-social-links"}><img src={Facebook}/> Example Charity</a>
+                    <a href={"https://www.instagram.com/"} className={"charity-social-links"}><img src={Instagram}/> @examplecharity</a>
+                    <a href={"https://www.tiktok.com/"} className={"charity-social-links"}><img src={Tiktok}/> @examplecharity</a>
+                    <a href={"https://www.snapchat.com/"} className={"charity-social-links"}><img src={Snapchat}/> examplecharity</a>
+                </div>
+                <div className={"charity-page-news"}>
+                    <div className={"newsfeed-parent"}>
+                        <h1>NEWSFEED</h1>
+                        <div className={"newsfeed-child"}>
+                            <div className={"newsfeed-item"}>
+                                <img src={ NewsImg1 } className={ "newsfeed-pic" } alt={"newsFeedPic"}/>
+                                <p>Afghan refugee children enrolling in norwegian nurseries 2 weeks after coming to norway. Prime minister hopes that these efforts will help stabalise the everyday life of the children and that they will integrate quicker.</p>
+                                <p className={"read-more-btn"}>Read more</p>
+                            </div>
+                            <div className={"newsfeed-item"}>
+                                <img src={ NewsImg2 } className={ "newsfeed-pic" } alt={"newsFeedPic"}/>
+                                <p>Hunting season has ended, yet people are still hunting animals illegally. Protecting nature is our responsibilty, and make sure that the next generation can enjoy wildnerness like us.</p>
+                                <p className={"read-more-btn"}>Read more</p>
+                            </div>
+                            <div className={"newsfeed-item"}>
+                                <img src={ NewsImg3 } className={ "newsfeed-pic" } alt={"newsFeedPic"}/>
+                                <p>Thanks to the support of charities, children like Akeem are able to partake in after school activities.
+
+                                    Children like Akeem would not be able to afford ...</p>
+                                <p className={"read-more-btn"}>Read more</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
@@ -37,7 +100,7 @@ export default function charityPage(){
 
     return (
         <div>
-            <ul className={"charity-parent-cont"}>
+            <ul className={"charity-page-container"}>
                 {data.map((charity, index) => (
                     <Charity key={index} charity={charity} />
                 ))}
