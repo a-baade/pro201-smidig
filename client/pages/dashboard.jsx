f18ed2ba363497a5a4330613e273cbb1e75f7753
ncdalen <78857275+ncdalen@users.noreply.github.com>
2022-05-31 12:05:38 +0200

    dashboard start (problem)

--- client/pages/dashboard.jsx
+++ client/pages/dashboard.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
-export function Dashboard() {
+
+export function Dashboard({ user }) {
   return (
       <div className={"dashboard-page"}>
           <div className={"dashboard-sidebar"}>
+              <p>{user.google.name}</p>
+              <p>My donations</p>
+              <p>My subscriptions</p>
+          </div>
+          <div className={"dashboard-main"}>
+              <div className={"dashboard-top-row"}>
 
+              </div>
           </div>
       </div>
   );
