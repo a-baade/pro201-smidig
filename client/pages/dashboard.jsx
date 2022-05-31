cc91128161e4a1a7ee807887c40f8dc347c68527
ncdalen <78857275+ncdalen@users.noreply.github.com>
2022-05-31 19:08:45 +0200

    basic layout

--- client/pages/dashboard.jsx
+++ client/pages/dashboard.jsx
@@ -1,48 +1,62 @@
 import React from "react";
+import { Chart2 } from "../components/charts";
+import { Chart1 } from "../components/charts";
 
 
 export function Dashboard({ user }) {
   return (
       <div className={"dashboard-page"}>
           <div className={"dashboard-sidebar"}>
-              <img src={user.google.picture} alt="Profile picture" className={"pfp"}/>
-              <p>{user.google.name}</p>
+              <div className={"dashboard-pfp-name"}>
+                  <img src={user.google.picture} alt="Profile picture" className={"dashboard-pfp"}/>
+                  <p>{user.google.name}</p>
+              </div>
               <p>My donations</p>
               <p>My subscriptions</p>
           </div>
           <div className={"dashboard-main"}>
               <div className={"dashboard-top-row"}>
-                <div className={"dasboard-info-box"}>
+                <div className={"dashboard-info-box"}>
                     <p>23 <br/> donation</p>
                 </div>
-                  <div className={"dasboard-info-box"}>
+                  <div className={"dashboard-info-box"}>
                       <p>5 <br/> subscriptions</p>
                   </div>
-                  <div className={"dasboard-info-box"}>
+                  <div className={"dashboard-info-box"}>
                       <p>750 000<br/> donated</p>
                   </div>
               </div>
               <div className={"dashboard-row-2"}>
                   <h2>Donation over time</h2>
-                  <div className={"over-time-chart"}></div>
-                  <div className={"over-time-sidebar"}>
-                      <button>Month</button>
-                      <div>
-                          <p>192 351 <br/> Avg per month</p>
+                  <div className={"chart-wrap"}>
+                      <div className={"chart2"}>
+                          <div>
+                              <Chart2/>
+                          </div>
+                      </div>
+                      <div className={"chart2-sidebar"}>
+                          <button>Month</button>
+                          <div className={"chart2-circle"}>
+                              <p>192 351 <br/> Avg per month</p>
+                          </div>
                       </div>
                   </div>
               </div>
               <div className={"dashboard-row-3"}>
                   <h2>Donasjoner</h2>
                   <h4>sammenlignet med lignende bedrifter</h4>
-                  <div className={"donations-chart"}></div>
-                  <div className={"donations-sidebar"}>
-                      <div className={"gjennomsnitt-wrap"}>
-                          <h4>25%</h4>
-                          <p>Bak gjennomsnittet</p>
+                  <div className={"chart-wrap"}>
+                      <div className={"donations-chart"}>
+                          <Chart1/>
                       </div>
-                      <div className={"omsetning-wrap"}>
-                          <p>Basert på firma sin omsetning</p>
+                      <div className={"chart1-sidebar"}>
+                          <div className={"chart1-circle"}>
+                              <p className={"percent-txt"}>25%</p>
+                              <p className={"chart1-circle-txt"}>Bak gjennomsnittet</p>
+                          </div>
+                          <div className={"chart1-textbox"}>
+                              <p>Basert på firma <br/> sin omsetning</p>
+                          </div>
                       </div>
                   </div>
               </div>
