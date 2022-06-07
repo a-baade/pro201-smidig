1b195e018e686a7b5fb1ee37166b3da63397fb1a
leaskagen <50326406+leaskagen@users.noreply.github.com>
2022-06-07 13:12:07 +0200

    clean up about us

--- client/pages/aboutUs.jsx
+++ client/pages/aboutUs.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import MainBg from "../pics/about-us-pictures/main-bg.png";
 import FirstLayerArt from "../pics/about-us-pictures/firstLayerArt.png";
 import TopLayerArt from "../pics/about-us-pictures/topLayerArt.png";
-
 import Water from "../pics/graphics/water.png";
 import Book from "../pics/graphics/book.png";
 import Cow from "../pics/graphics/cow.png";
@@ -19,7 +18,7 @@ export function AboutUs() {
                 <img src={MainBg} className={"main-bg-pic"} alt={"mainBg"} />
             </div>
             <div className={"second-row-box"}>
-                <div className={"knowledge-text-box"}>
+                <div className={"knowledge-text-box custom-shadow"}>
                     <p className={"our-mission"}>OUR MISSION</p>
                     <h1 className={"knowledge-power custom-font-knowledge"}>KNOWLEDGE = POWER</h1>
                     <div className={"power-text"}>
@@ -32,11 +31,11 @@ export function AboutUs() {
                             streams for nonprofits, and create deeper relationships between
                             donor and nonprofit.
                         </p>
-                            <p>
-                                {" "}
-                                We help implement solid strategies for impactful and sustainable
-                                giving, all in line with the UN SDGs.
-                            </p>
+                        <p>
+                            {" "}
+                            We help implement solid strategies for impactful and sustainable
+                            giving, all in line with the UN SDGs.
+                        </p>
                     </div>
                 </div>
                 <div className={"art-box"}>
@@ -50,7 +49,6 @@ export function AboutUs() {
                         className={"top-layer-art"}
                         alt={"topLayer"}
                     />
-
                 </div>
             </div>
             <p className={"our-mission mission"}>OUR MISSION</p>
@@ -74,7 +72,7 @@ export function AboutUs() {
                 </div>
                 <div className={"small-box light custom-shadow"}>
                     <img src={Sustainability} className={"graphics"} />
-                    <p className={"small-title custom-font-knowledge"}>Subsainable cities & communinites</p>
+                    <p className={"small-title "}>Sustainable cities & communities</p>
                     <p className={"small-txt"}>
                         Make cities and human settlements inclusive, safe, resilient and
                         sustainable
@@ -82,21 +80,21 @@ export function AboutUs() {
                 </div>
                 <div className={"small-box dark custom-shadow"}>
                     <img src={Consumption} className={"graphics"} />
-                    <p className={"small-title custom-font-knowledge"}>Responsible consumption & production</p>
+                    <p className={"small-title "}>Responsible consumption & production</p>
                     <p className={"small-txt"}>
                         Ensure sustainable consumption and production patterns
                     </p>
                 </div>
                 <div className={"small-box light custom-shadow"}>
                     <img src={ClimateAction} className={"graphics"} />
-                    <p className={"small-title custom-font-knowledge"}>Climate action</p>
+                    <p className={"small-title "}>Climate action</p>
                     <p className={"small-txt"}>
                         Take urgent action to combat climate change and its impacts
                     </p>
                 </div>
                 <div className={"small-box dark custom-shadow"}>
                     <img src={Whale} className={"graphics"} />
-                    <p className={"small-title custom-font-knowledge"}>Life below water</p>
+                    <p className={"small-title "}>Life below water</p>
                     <p className={"small-txt"}>
                         Conserve and sustainably use the oceans, seas and marine resources
                         for sustainable development
@@ -104,7 +102,7 @@ export function AboutUs() {
                 </div>
                 <div className={"small-box light custom-shadow"}>
                     <img src={Cow} className={"graphics"} />
-                    <p className={"small-title custom-font-knowledge"}>Life on land</p>
+                    <p className={"small-title "}>Life on land</p>
                     <p className={"small-txt"}>
                         Protect, restore and promote sustainable use of terrestrial
                         ecosystems, sustainably manage forests, combat desertification, and
@@ -113,7 +111,7 @@ export function AboutUs() {
                 </div>
                 <div className={"small-box dark custom-shadow"}>
                     <img src={Flower} className={"graphics"} />
-                    <p className={"un-goals-title custom-font-knowledge"}>THE UN SDG GOALS</p>
+                    <p className={"small-title"}>THE UN SDG GOALS</p>
                 </div>
             </div>
         </div>
